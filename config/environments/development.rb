fd9137caa8a2f053143899535a53e61fa03c181e
usmanFrontDev <usmanrajput4488@gmail.com>
2025-11-02 21:07:09 +0500

    Initial commit - Rails React Landing Page

---
+++ config/environments/development.rb
@@ -0,0 +1,17 @@
+require "active_support/core_ext/integer/time"
+
+Rails.application.configure do
+  config.enable_reloading = true
+  config.eager_load = false
+  config.consider_all_requests_local = true
+  config.server_timing = true
+  
+  config.action_controller.perform_caching = false
+  config.cache_store = :null_store
+  
+  config.active_support.deprecation = :log
+  config.active_support.disallowed_deprecation = :raise
+  config.active_support.disallowed_deprecation_warnings = []
+  
+  config.assets.quiet = true
+end
